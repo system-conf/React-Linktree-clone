5f55bf2828241518c86b77051be69ffb74b22d8e
system.conf <suleyman.talha.duman0@gmail.com>
2024-05-25 05:12:28 +0300

    update

--- src/firebaseconf.js
+++ src/firebaseconf.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+require('./.env').config();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAOe2nAVMwdHOR7jHqtYEaaTldD74fQdXc",
-    authDomain: "barmen-3ecea.firebaseapp.com",
-    databaseURL: "https://barmen-3ecea-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "barmen-3ecea",
-    storageBucket: "barmen-3ecea.appspot.com",
-    messagingSenderId: "977466560954",
-    appId: "1:977466560954:web:fd0a6681d83c471d518d83",
-    measurementId: "G-42T80Z49EV"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
